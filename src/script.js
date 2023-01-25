058a9a835427c8dc6ad6d0d0bf4a71286ebd3090
jotadeoliveira <csjfonf@hotmail.com>
2023-01-25 17:44:22 -0300

    Fixed the app on purpose

--- src/script.js
+++ src/script.js
@@ -7,11 +7,11 @@ document.querySelector('#phone-checkbox')
   .addEventListener('change', function() {
     if (this.checked) {
       phoneLabelSpan.style.display = 'inline'
-      //phoneField.required = !isPhoneRequired
+      phoneField.required = !isPhoneRequired
       isPhoneRequired = !isPhoneRequired
     } else {
       phoneLabelSpan.style.display = 'none'
-      //phoneField.required = !isPhoneRequired
+      phoneField.required = !isPhoneRequired
       isPhoneRequired = !isPhoneRequired
     }
   })
@@ -19,19 +19,19 @@ document.querySelector('#phone-checkbox')
 document.querySelector('button[type="submit"]')
   .addEventListener('click', function(event) {
     event.preventDefault()
-    //const firstNameField = document.getElementById('firstName')
+    const firstNameField = document.getElementById('firstName')
     const lastNameField = document.getElementById('lastName')
-    //const emailField = document.getElementById('email')
+    const emailField = document.getElementById('email')
     const textareaField = document.getElementById('open-text-area')
     const productField = document.getElementById('product')
-    //const helpRadio = document.querySelector('input[value="ajuda"]')
+    const helpRadio = document.querySelector('input[value="ajuda"]')
     const emailCheckbox = document.getElementById('email-checkbox')
     const phoneCheckbox = document.getElementById('phone-checkbox')
     const fileField = document.querySelector('input[type="file"]')
     const successMessage = document.querySelector('.success')
-    //if (!firstNameField.value || !lastNameField.value || !emailField.value || !textareaField.value) {
-    //  return showAndHideErrorMessage()
-    //}
+    if (!firstNameField.value || !lastNameField.value || !emailField.value || !textareaField.value) {
+      return showAndHideErrorMessage()
+    }
     if (isPhoneRequired && !phoneField.value) {
       return showAndHideErrorMessage()
     }
